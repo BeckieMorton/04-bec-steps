00df09512f6d6abba9ac9ff2611bca5762d7ab43
BeckieMorton <beckam11@gmail.com>
2024-03-25 11:08:52 +1100

    fixing repo new commit with locale files

---
+++ src/App.jsx
@@ -0,0 +1,79 @@
+import { useState } from "react";
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
+import React from "react";
+
+export const App = () => {
+  return (
+    <div>
+      <Steps />
+    </div>
+  );
+};
+
+export const Steps = () => {
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+
+  const handlePrevious = () => {
+    if (step > 1) setStep((currentStep) => currentStep - 1);
+  };
+
+  const handleNext = () => {
+    if (step < 3) setStep(step + 1);
+  };
+
+  return (
+    <>
+      <button className="close" onClick={() => setIsOpen((is) => !is)}>
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
+
+          <StepMessage step={step}>{messages[step - 1]}</StepMessage>
+
+          <div className="buttons">
+            <Button textColor="#fff" bgColor="#7950f2" onClick={handlePrevious}>
+              <span>👈</span>Previous
+            </Button>
+            <Button textColor="#fff" bgColor="#7950f2" onClick={handleNext}>
+              Next <span>👉</span>
+            </Button>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+function StepMessage({ step, children }) {
+  return (
+    <div className="message">
+      <h3>Step {step}: </h3>
+      {children}
+    </div>
+  );
+}
+
+//for the component to get access to what is between the opening and closing tags above we use children props, everything recives this automatically and it is what is between the opening and closing tags (if used!) 'children' is a predefined key word in react!
+function Button({ textColor, bgColor, onClick, children }) {
+  return (
+    <button
+      style={{ backgroundColor: bgColor, color: textColor }}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+}
